1f755e08ee4b24f5be8407b2b2b62410c32a7096
unknown <purtiaggarwal1997@gmail.com>
2020-06-17 00:53:32 +0530

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -14,22 +14,8 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-// store.subscribe(() => {
-//     const state = store.getState();
-//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//     console.log(visibleExpenses);
-// });
 
 
-store.dispatch(addExpenses({ description: 'Water bill', amount: 20000, createdAt: 1000 }));
-store.dispatch(addExpenses({ description: 'rent', amount: 20, createdAt:20000 }));
-store.dispatch(addExpenses({ description: 'gas bill', amount: 2000, createdAt:3000 }));
-
- 
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
